07920787fd889a3528218cf0a75806784717261c
pengf@replay45 <17551496+shiba2046@users.noreply.github.com>
2021-12-17 13:45:59 +0800

    Added powerlevel10k theme

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,6 +1,8 @@
 #/bin/env zsh
 
 
+
+# git clone git@github.com:shiba2046/linux_tools.git
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -16,6 +18,11 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 
 # Enable Plugins
 omz plugin enable zsh-autosuggestions zsh-syntax-highlighting python pip sudo tmux
+
+# Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+omz theme set powerlevel10k/powerlevel10k
+
 
 
 
